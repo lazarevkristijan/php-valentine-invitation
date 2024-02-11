2f5255eb89f069465254a7b0e91384ad7e350a57
lazarevkristijan <lazarevkristijan@gmail.com>
2024-02-11 15:19:34 +0100

    add basic files with some styles

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Interview</title>
+    <link rel="stylesheet" href="index.css">
+</head>
+<body>
+    <main>
+        <h1>
+            Will you be my valentine?
+        </h1>
+
+        <button class="yes">yes</button>
+        <button class="no">no</button>
+    </main>
+</body>
+</html>
+
+<?php
+
+?>
