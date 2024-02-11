026ad9f58044131e4032b0acd3fb129a7e990840
lazarevkristijan <lazarevkristijan@gmail.com>
2024-02-11 15:45:32 +0100

    add form submit

--- index.php
+++ index.php
@@ -8,16 +8,20 @@
 </head>
 <body>
     <main>
-        <h1>
-            Will you be my valentine?
-        </h1>
+        <form action="index.php" method="post">
+            <h1>
+                Will you be my valentine?
+            </h1>
 
-        <button class="yes">yes</button>
-        <button class="no">no</button>
+            <button type="submit" class="yes">yes</button>
+            <button type="button" class="no">no</button>
+        </form>
     </main>
 </body>
 </html>
 
 <?php
-
+    if($_SERVER["REQUEST_METHOD"] == 'POST') {
+        echo "<p>Thank you</p>";
+    }
 ?>
